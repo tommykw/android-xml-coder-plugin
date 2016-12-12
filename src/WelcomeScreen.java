a38ac9d3f6baf37576e1e9dd7e5eb3a0c9e81179
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-12-13 08:58:57 +0900

    Recompose project

---
+++ src/WelcomeScreen.java
@@ -0,0 +1,13 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by tommy on 2016/12/13.
+ */
+public class WelcomeScreen extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
